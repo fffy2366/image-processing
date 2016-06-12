5d8bce6519e0a09a5146320ddb7606ea37a28163
Frank <fengxuting@gmail.com>
2016-06-12 16:15:59 +0800

    Update nude img resize 300

--- nudedetect.py
+++ nudedetect.py
@@ -8,12 +8,56 @@ import sys
 from nude import Nude
 import time
 import cv2
+from PIL import Image as image
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 print('start waiting', time.strftime('%H:%M:%S'), '\n<br/>')
-file = "public/uploads/nude/" + sys.argv[1]
+IMAGE_DIR = "public/uploads/nude/"
+file = IMAGE_DIR + sys.argv[1]
+disImg = IMAGE_DIR +"nude_"+sys.argv[1]
+# 图片如果宽或高大于300则等比例压缩
+def resizeImg(**args):
+    args_key = {'ori_img': '', 'dst_img': '', 'dst_w': '', 'dst_h': '', 'save_q': 75}
+    arg = {}
+    for key in args_key:
+        if key in args:
+            arg[key] = args[key]
 
+    im = image.open(arg['ori_img'])
+    ori_w, ori_h = im.size
+    if (ori_w and ori_w > arg['dst_w']) or (ori_h and ori_h > arg['dst_h']):
+
+        widthRatio = heightRatio = None
+        ratio = 1
+        if (ori_w and ori_w > arg['dst_w']) or (ori_h and ori_h > arg['dst_h']):
+            if arg['dst_w'] and ori_w > arg['dst_w']:
+                widthRatio = float(arg['dst_w']) / ori_w  # 正确获取小数的方式
+            if arg['dst_h'] and ori_h > arg['dst_h']:
+                heightRatio = float(arg['dst_h']) / ori_h
+
+            if widthRatio and heightRatio:
+                if widthRatio < heightRatio:
+                    ratio = widthRatio
+                else:
+                    ratio = heightRatio
+
+            if widthRatio and not heightRatio:
+                ratio = widthRatio
+            if heightRatio and not widthRatio:
+                ratio = heightRatio
+
+            newWidth = int(ori_w * ratio)
+            newHeight = int(ori_h * ratio)
+        else:
+            newWidth = ori_w
+            newHeight = ori_h
+
+        im.resize((newWidth, newHeight), image.ANTIALIAS).save(arg['dst_img'], quality=arg['save_q'])
+        return arg['dst_img']
+    else:
+        return arg['ori_img']
+#图像压缩
 
 # 人脸识别 排除人脸
 def facedetect(file):
@@ -46,9 +90,11 @@ def facedetect(file):
 
     return faces
 
-faces = facedetect(file)
-n = Nude(file)
-n.setFaces(faces) ;
+newImg = resizeImg(ori_img=file,dst_img=disImg,dst_w=300,dst_h=300,save_q=100)
+
+faces = facedetect(newImg)
+n = Nude(newImg)
+n.setFaces(faces)
 # n.resize(1000,1000)
 n.parse()
 
